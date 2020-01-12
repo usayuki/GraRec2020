27f17d8f669310426029258ce2b86bed191fbcc7
YokoTanaka0711 <prymjou@gmail.com>
2020-01-12 18:17:08 +0900

    UIDragInteractionDelegate、UIDropInteractionDelegateを実装したいです、、、

---
+++ PencilKitDraw/DropModel2.swift
@@ -0,0 +1,33 @@
+//
+//  DropModel2.swift
+//  PencilKitDraw
+//
+//  Created by 田中陽子 on 2020/01/12.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class DropmodelController {
+
+//    let dropInteraction = UIDropInteraction(delegate: self)
+//    view.addInteraction(dropInteraction)
+//    
+//    func dropInteraction(
+//         _ interaction: UIDropInteraction,
+//         sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+//
+//         let dropLocation = session.location(in: view)
+//         let operation: UIDropOperation
+//
+//         if imageView.frame.contains(dropLocation) {
+//           operation = session.localDragSession == nil ? .copy : .move
+//         } else {
+//           operation = .cancel
+//         }
+//
+//         return UIDropProposal(operation: operation)
+//       }
+    
+}
+    
--- PencilKitDraw/FaceItemCollectionViewCell.swift
+++ PencilKitDraw/FaceItemCollectionViewCell.swift
@@ -9,31 +9,36 @@
 import UIKit
 
 //ドラッグするもの（＝UICollectionViewCellを継承した子クラス）
-class FaceItemCollectionViewCell: UICollectionViewCell,UIDragInteractionDelegate {
+//class FaceItemCollectionViewCell: UICollectionViewCell,UIDragInteractionDelegate {
+//func dragInteraction(_ interaction: UIDragInteraction, itemsForBeginning session: UIDragSession) -> [UIDragItem] {
+////    <#code#>
+//}
+
+//
+//    @IBOutlet weak var itemImage: UIImageView!
+////
+////
+//    let dragInteraction = UIDragInteraction(delegate: self)
+//    itemImage.addInteraction(dragInteraction)
+//
+//    //    let dragDelegate: UIDragInteractionDelegate = ...
+//    //    let dragInteraction = UIDragInteraction(delegate: dragDelegate)
+//    //    dragInteraction.isEnabled = true    // iPhoneの場合はデフォルトがfalseになっている
+//    //    view.addInteraction(dragInteraction)
+//
+//
+//    func dragInteraction(
+//        _ interaction: UIDragInteraction,
+//        itemsForBeginning session: UIDragSession) -> [UIDragItem] {
+//
+//        guard let image = imageView.image else { return [] }
+//
+//
+//
+//        let provider = NSItemProvider(object: image)
+//        let item = UIDragItem(itemProvider: provider)
+//        item.localObject = image
+//        return [item]
+//    }
     
-    @IBOutlet weak var itemImage: UIImageView!
-    
-    let dragInteraction = UIDragInteraction(delegate: self)
-    imageView.addInteraction(dragInteraction)
-    
-    //    let dragDelegate: UIDragInteractionDelegate = ...
-    //    let dragInteraction = UIDragInteraction(delegate: dragDelegate)
-    //    dragInteraction.isEnabled = true    // iPhoneの場合はデフォルトがfalseになっている
-    //    view.addInteraction(dragInteraction)
-    
-    
-    func dragInteraction(
-        _ interaction: UIDragInteraction,
-        itemsForBeginning session: UIDragSession) -> [UIDragItem] {
-        
-        guard let image = imageView.image else { return [] }
-        
-        
-        
-        let provider = NSItemProvider(object: image)
-        let item = UIDragItem(itemProvider: provider)
-        item.localObject = image
-        return [item]
-    }
-    
-}
+//}
