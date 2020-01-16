bb124d74f53bcadd32b9fbd7f88436069f0d00d9
usayuki <masapp.01a@gmail.com>
2020-01-16 17:29:21 +0900

    ドロップ位置に配置する

--- PencilKitDraw/DrawingViewController.swift
+++ PencilKitDraw/DrawingViewController.swift
@@ -307,14 +307,15 @@ extension DrawingViewController: UIDropInteractionDelegate {
 
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
         session.loadObjects(ofClass: UIImage.self, completion: { [weak self] imageItems in
-            guard let images = imageItems as? [UIImage] else { return }
-            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+            guard let images = imageItems as? [UIImage], let self = self else { return }
+            let point = session.location(in: self.canvasView)
+            let imageView = UIImageView(frame: CGRect(x: point.x, y: point.y, width: 200, height: 200))
             imageView.clipsToBounds = true
             imageView.contentMode = .scaleAspectFill
             imageView.image = images.first
-            self?.canvasView.addSubview(imageView)
+            self.canvasView.addSubview(imageView)
             
-            self?.dismissPopover()
+            self.dismissPopover()
         })
     }
 }
